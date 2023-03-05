f835f12563a569cb5451f03f3ae844d58584986b
pawelprzegon <p.przegon@gmail.com>
2023-03-05 22:18:00 +0100

    create edit view all and one raport ready

--- frontend/src/templates/allRaportsView.js
+++ frontend/src/templates/allRaportsView.js
@@ -1,55 +1,53 @@
 import {url} from "../common/data/index.js"
-import {addPaginate} from '../common/paginate/index.js'
+import {Slider} from '../features/swiper/slider.js'
 import {Brick} from '../features/brick/index.js'
 import {callApiGet, tokenRefresh} from '../features/endpoints/index.js'
 import {hideloader} from '../features/loading/loading.js'
-import { navigateTo } from "../js/index.js"
+import {navigateTo} from "../js/index.js"
 
 import AbstractView from "./AbstractView.js";
 
 export default class extends AbstractView{
     constructor(params){
         super(params);
+
+        this.bricks = []
         window.scrollTo(0,0);
+        $("body").css("overflow", "hidden");
+        
+        this.api_url = url+"raports/"
         if (this.params.username){
             this.setTitle("Moje raporty")
+            this.api_url = url+"raports/"+this.params.username
         }else{
             this.setTitle("Raporty")
         }
+        this.prepare();
     }
 
-    async getData(){
-        // console.log(this.params)
-        document.getElementById('loading').style.visibility = 'visible';
-
-        let api_url = url+"raports/"
-        if (this.params.username){
-            api_url = url+"raports/"+this.params.username
-        }
+    prepare(){
+        const FormData = document.querySelector('#form-data');
+        FormData.innerHTML = '';
 
         let theme = document.getElementById('theme')
         theme.setAttribute('href', "/../src/css/allRaports.css");
-
         
+        document.getElementById('loading').style.visibility = 'visible';
+    }
 
-        $("body").css("overflow", "hidden");
-        const raportList = document.querySelector('#raport');
-        raportList.innerHTML = '';
-            
-        const clearForm = document.querySelector('#form');
-        clearForm.innerHTML='';
+    async getData(){
 
         try {
-            let [response, status] = await callApiGet(api_url);
+            let [response, status] = await callApiGet(this.api_url);
             if (response.detail && response.detail == "Not authenticated"){
                 console.log('refreshing token')
                 let refTokenResponse = await tokenRefresh();
                 console.log(refTokenResponse)
                 if (refTokenResponse[1] == 200){
-                    let [response, status] = await callApiGet(api_url);
+                    let [response, status] = await callApiGet(this.api_url);
                     if (status == 200){
-                        console.log(response)
                         this.show(response);
+                        Slider();
                     }else{
                         document.getElementById('err').innerHTML = `
                         <h1>${status}</h1>
@@ -58,16 +56,18 @@ export default class extends AbstractView{
                     }
                     
                 }else{
+                    hideloader();
                     navigateTo('/login')
                 }
                 
             }else{
                 hideloader();
-                // console.log('data: '+response);
+                console.log(response)
                 this.show(response);
-                addPaginate();
+                Slider();
             }
         }catch (error){
+            hideloader();
             console.log(error)
             navigateTo('/login')
         }
@@ -75,16 +75,43 @@ export default class extends AbstractView{
 
 
     show(data) {
+
+        const swiperContent = document.createElement('div')
+        swiperContent.classList.add('slide-content')
+
         const raportList = document.querySelector('#raport');
         raportList.classList.remove('single')
         raportList.innerHTML = '';
 
+        const raportListSwiper = document.createElement('div')
+        raportListSwiper.innerHTML=''
+        raportListSwiper.classList.add('swiper-wrapper')
+        
+
         data.forEach(each => {
+            
             let raportInfoGrid = new Brick(each)
             let brick = raportInfoGrid.getBrick()
-            raportList.appendChild(brick);
+            raportListSwiper.appendChild(brick);
         });
+
+        const next = document.createElement('div')
+        next.classList.add('swiper-button-next')
+        const prev = document.createElement('div')
+        prev.classList.add('swiper-button-prev')
+        const paginate = document.createElement('div')
+        paginate.classList.add('swiper-pagination')
+
+        
+        swiperContent.appendChild(raportListSwiper)
+        swiperContent.appendChild(next)
+        swiperContent.appendChild(prev)
+        swiperContent.appendChild(paginate)
+        
+
+        raportList.appendChild(swiperContent)
+
     }
+
+    
 }
-
-
