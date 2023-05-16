c15f136e869e63253d7c0386eb1c87a763ef3904
pawelprzegon <p.przegon@gmail.com>
2023-05-16 22:39:25 +0200

    changed css for mobile

--- server.js
+++ server.js
@@ -10,4 +10,7 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
