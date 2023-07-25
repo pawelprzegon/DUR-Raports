cba63a27de6d0a5f36693f76e84a157e9751e026
pawelprzegon <p.przegon@gmail.com>
2023-07-25 13:08:13 +0200

    refacor: authView with auth css

--- front/frontend/src/templates/authView.js
+++ front/frontend/src/templates/authView.js
@@ -19,15 +19,21 @@ export default class extends AbstractView {
       .getElementById('theme')
       .setAttribute('href', '../src/css/auth.css');
     document.getElementById('app-header').setAttribute('style', 'height:0%');
-    document.getElementById('alerts').setAttribute('style', 'height:0%');
+    // document.getElementById('alerts').setAttribute('style', 'height:0%');
   }
 
-  async getData() {
+  getData() {
     hideloader();
     this.setTitle('Login');
     this.css();
-    this.set_background_pic();
     this.container.innerHTML = '';
+    this.createForm();
+    this.createInputs();
+    this.createSubmit();
+    this.createLinks();
+  }
+
+  createForm() {
     this.formField = document.createElement('form');
     this.formField.action = '#';
     this.formField.id = 'form';
@@ -37,161 +43,76 @@ export default class extends AbstractView {
     this.header.innerText = 'Login';
     this.header.id = 'header';
     this.formField.appendChild(this.header);
+
+    // Server response section
     this.responseBox = document.createElement('div');
     this.responseBox.id = 'responseBox';
     this.formField.appendChild(this.responseBox);
-
-    // Errors section
     this.responseStatus = document.createElement('p');
     this.responseStatus.innerText = '';
     this.responseStatus.classList.add('response-error');
     this.responseData = document.createElement('p');
     this.responseData.innerText = '';
     this.responseData.classList.add('response-error');
-
     this.responseBox.appendChild(this.responseStatus);
     this.responseBox.appendChild(this.responseData);
+  }
 
+  createInputs() {
     // inputs section
-
     this.data.forEach((each) => {
-      this.elemBox = document.createElement('div');
-      this.elemBox.classList.add('input-control');
-      this.elemBox.id = 'div-' + each;
-      this.elemLabel = document.createElement('p');
-      this.elemLabel.innerText = each;
-      this.elemText = document.createElement('input');
+      const input = document.createElement('div');
+      input.classList.add('input-control');
+      input.id = 'div-' + each;
+      const label = document.createElement('p');
+      label.innerText = each;
+      const text = document.createElement('input');
+      text.name = each;
+      text.id = each;
+      input.appendChild(label);
+      input.appendChild(text);
       if (each === 'password' || each === 'confirm') {
-        this.elemText.type = 'password';
-        this.checkboxBox = document.createElement('div');
-        this.checkboxBox.classList.add('show-passwrd');
-        this.checkbox = document.createElement('input');
+        text.type = 'password';
+        const [checkboxBox, checkbox] = this.createCheckBoxes(each);
 
-        this.checkbox.type = 'checkbox';
-        this.checkbox.onclick = function () {
-          showPassword(each);
-        };
-        this.checkboxText = document.createElement('small');
-        this.checkboxText.innerText = 'Show ' + each;
-        this.checkboxBox.appendChild(this.checkbox);
-        this.checkboxBox.appendChild(this.checkboxText);
+        if (checkbox != undefined) {
+          input.appendChild(checkboxBox);
+        }
       } else {
-        this.elemText.type = 'text';
+        text.type = 'text';
       }
-      this.elemText.name = each;
-      this.elemText.id = each;
-      this.Err = document.createElement('div');
-      this.Err.classList.add('error');
-      this.elemBox.appendChild(this.elemLabel);
-      this.elemBox.appendChild(this.elemText);
-      if (this.checkbox != undefined) {
-        this.elemBox.appendChild(this.checkboxBox);
-      }
-      this.elemBox.appendChild(this.Err);
-      this.formField.appendChild(this.elemBox);
-    });
 
-    this.submitButtonBox = document.createElement('div');
+      const err = document.createElement('div');
+      err.classList.add('error');
+
+      input.appendChild(err);
+      this.formField.appendChild(input);
+    });
+  }
+
+  createCheckBoxes(element) {
+    const checkboxBox = document.createElement('div');
+    checkboxBox.classList.add('show-passwrd');
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.onclick = function () {
+      showPassword(element);
+    };
+    const checkboxText = document.createElement('small');
+    checkboxText.innerText = 'Show ' + element;
+    checkboxBox.appendChild(checkbox);
+    checkboxBox.appendChild(checkboxText);
+    return [checkboxBox, checkbox];
+  }
+
+  createSubmit() {
+    const submitButtonBox = document.createElement('div');
     this.submitButton = document.createElement('button');
     this.submitButton.classList.add('submit');
     this.submitButton.innerText = 'Login';
 
-    this.submitButtonBox.appendChild(this.submitButton);
-    this.formField.appendChild(this.submitButtonBox);
-
-    this.loginSignupBox = document.createElement('div');
-    this.loginSignupBox.classList.add('loginSignup-box');
-    this.prefix = document.createElement('p');
-    this.prefix.innerText = "Don't have an account?";
-    this.loginSignupLink = document.createElement('a');
-    this.loginSignupLink.innerText = 'SignUp';
-    this.loginSignupLink.id = 'loginSignup';
-    this.loginSignupLink.classList.add('loginSignup');
-    this.loginSignupBox.appendChild(this.prefix);
-    this.loginSignupBox.appendChild(this.loginSignupLink);
-
-    this.formField.appendChild(this.loginSignupBox);
-
-    this.resetPasswdBox = document.createElement('div');
-    this.resetPasswdBox.classList.add('resetPasswdBox');
-    this.resetPasswdBox.id = 'resetPasswdBox';
-    this.resetPasswdLink = document.createElement('a');
-    this.resetPasswdLink.innerText = 'Forget password?';
-    this.resetPasswdBox.appendChild(this.resetPasswdLink);
-    this.formField.appendChild(this.resetPasswdBox);
-
-    this.container.appendChild(this.formField);
-
-    document.getElementById('div-email').classList.add('hidden-element');
-    document.getElementById('div-confirm').classList.add('hidden-element');
-    // console.log(this.loginSignupLink.innerText)
-
-    //Login SignUp
-    this.loginSignupLink.addEventListener(
-      'click',
-      (e) => {
-        document.getElementById('responseBox').innerHTML = '';
-        let inputs = document.getElementsByClassName('input-control');
-        Array.from(inputs).forEach((el) => {
-          el.children[1].value = '';
-        });
-
-        if (this.loginSignupLink.innerText == 'SignUp') {
-          document
-            .getElementById('div-email')
-            .classList.remove('hidden-element');
-          document
-            .getElementById('div-confirm')
-            .classList.remove('hidden-element');
-          document
-            .getElementById('div-username')
-            .classList.remove('hidden-element');
-          document
-            .getElementById('div-password')
-            .classList.remove('hidden-element');
-          this.header.innerText = 'Register';
-          this.submitButton.innerText = 'Register';
-          this.prefix.innerText = 'Already have an account?';
-          this.loginSignupLink.innerText = 'Login';
-        } else {
-          document.getElementById('div-email').classList.add('hidden-element');
-          document
-            .getElementById('div-confirm')
-            .classList.add('hidden-element');
-          document
-            .getElementById('div-username')
-            .classList.remove('hidden-element');
-          document
-            .getElementById('div-password')
-            .classList.remove('hidden-element');
-          document
-            .getElementById('resetPasswdBox')
-            .classList.remove('hidden-element');
-          this.header.innerText = 'Login';
-          this.submitButton.innerText = 'Login';
-          this.prefix.innerText = "Don't have an account?";
-          this.loginSignupLink.innerText = 'SignUp';
-        }
-      },
-      false
-    );
-
-    //odzyskiwanie hasła
-    this.resetPasswdLink.addEventListener('click', (e) => {
-      document.getElementById('responseBox').innerHTML = '';
-      let inputs = document.getElementsByClassName('input-control');
-      Array.from(inputs).forEach((el) => {
-        el.children[1].value = '';
-      });
-      document.getElementById('div-username').classList.add('hidden-element');
-      document.getElementById('div-password').classList.add('hidden-element');
-      document.getElementById('div-confirm').classList.add('hidden-element');
-      document.getElementById('div-email').classList.remove('hidden-element');
-      this.header.innerText = 'Reset';
-      this.submitButton.innerText = 'Reset';
-      this.loginSignupLink.innerText = 'Login';
-    });
-
+    submitButtonBox.appendChild(this.submitButton);
+    this.formField.appendChild(submitButtonBox);
     //Submit
     this.submitButton.addEventListener('click', (e) => {
       e.preventDefault();
@@ -218,23 +139,117 @@ export default class extends AbstractView {
     });
   }
 
-  set_background_pic() {
-    const body = document.querySelector('body');
+  createLinks() {
+    const loginSignupBox = document.createElement('div');
+    loginSignupBox.classList.add('loginSignup-box');
+    const accountPrefix = document.createElement('p');
+    accountPrefix.innerText = "Don't have an account?";
+    this.loginSignupLink = document.createElement('a');
+    this.loginSignupLink.innerText = 'SignUp';
+    this.loginSignupLink.id = 'loginSignup';
+    this.loginSignupLink.classList.add('loginSignup');
+    loginSignupBox.appendChild(accountPrefix);
+    loginSignupBox.appendChild(this.loginSignupLink);
+    this.formField.appendChild(loginSignupBox);
+
+    const resetPasswdBox = document.createElement('div');
+    resetPasswdBox.classList.add('resetPasswdBox');
+    resetPasswdBox.id = 'resetPasswdBox';
+    const resetPasswdPrefix = document.createElement('p');
+    resetPasswdPrefix.innerText = 'Forgot password?';
+    this.resetPasswdLink = document.createElement('a');
+    this.resetPasswdLink.innerText = 'Reset your password!';
+    resetPasswdBox.appendChild(resetPasswdPrefix);
+    resetPasswdBox.appendChild(this.resetPasswdLink);
+    this.formField.appendChild(resetPasswdBox);
+
+    this.container.appendChild(this.formField);
+
+    document.getElementById('div-email').classList.add('hidden-element');
+    document.getElementById('div-confirm').classList.add('hidden-element');
+    // console.log(this.loginSignupLink.innerText)
+    //Login SignUp
+    this.loginSignupLink.addEventListener(
+      'click',
+      (e) => {
+        document.getElementById('responseBox').innerHTML = '';
+        let inputs = document.getElementsByClassName('input-control');
+        Array.from(inputs).forEach((el) => {
+          el.children[1].value = '';
+        });
+
+        if (this.loginSignupLink.innerText == 'SignUp') {
+          document
+            .getElementById('div-email')
+            .classList.remove('hidden-element');
+          document
+            .getElementById('div-confirm')
+            .classList.remove('hidden-element');
+          document
+            .getElementById('div-username')
+            .classList.remove('hidden-element');
+          document
+            .getElementById('div-password')
+            .classList.remove('hidden-element');
+          this.header.innerText = 'Register';
+          this.submitButton.innerText = 'Register';
+          accountPrefix.innerText = 'Already have an account?';
+          this.loginSignupLink.innerText = 'Login';
+        } else {
+          document.getElementById('div-email').classList.add('hidden-element');
+          document
+            .getElementById('div-confirm')
+            .classList.add('hidden-element');
+          document
+            .getElementById('div-username')
+            .classList.remove('hidden-element');
+          document
+            .getElementById('div-password')
+            .classList.remove('hidden-element');
+          document
+            .getElementById('resetPasswdBox')
+            .classList.remove('hidden-element');
+          this.header.innerText = 'Login';
+          this.submitButton.innerText = 'Login';
+          accountPrefix.innerText = "Don't have an account?";
+          this.loginSignupLink.innerText = 'SignUp';
+        }
+      },
+      false
+    );
+    //odzyskiwanie hasła
+    this.resetPasswdLink.addEventListener('click', (e) => {
+      document.getElementById('responseBox').innerHTML = '';
+      let inputs = document.getElementsByClassName('input-control');
+      Array.from(inputs).forEach((el) => {
+        el.children[1].value = '';
+      });
+      document.getElementById('div-username').classList.add('hidden-element');
+      document.getElementById('div-password').classList.add('hidden-element');
+      document.getElementById('div-confirm').classList.add('hidden-element');
+      document.getElementById('div-email').classList.remove('hidden-element');
+      this.header.innerText = 'Reset';
+      submitButton.innerText = 'Reset';
+      this.loginSignupLink.innerText = 'Login';
+    });
   }
 
   validateInputs(formDataObj) {
     for (const [key, value] of Object.entries(formDataObj)) {
       let obj = document.getElementById(key);
-      value.trim();
-      if (value === '') {
+      let trimedValue = value.trim();
+      console.log(trimedValue);
+      if (trimedValue === '') {
         this.setError(obj, key + ' jest wymagany');
-      } else if (key === 'email' && !this.isValidEmail(value)) {
+      } else if (value.indexOf(' ') !== -1) {
+        this.setError(obj, 'usuń spacje');
+      } else if (key === 'email' && !this.isValidEmail(trimedValue)) {
         this.setError(obj, 'nieprawidłowy adres email');
-      } else if (key === 'password' && value.length < 6) {
+      } else if (key === 'password' && trimedValue.length < 6) {
         this.setError(obj, 'hasło musi mieć conajmniej 6 znaków');
       } else if (
         key === 'confirm' &&
-        value !== document.getElementById('password').value
+        trimedValue !== document.getElementById('password').value
       ) {
         this.setError(obj, 'hasła nie pasują');
       } else {
@@ -304,7 +319,6 @@ export default class extends AbstractView {
       alerts('Success', response.message, 'alert-green');
     } else {
       this.responseBox.innerHTML = '';
-      this.response = document.createElement('div');
       this.responseStatus = document.createElement('p');
       // this.responseStatus.innerText = status;
       this.responseStatus.classList.add('response-error');
@@ -345,7 +359,6 @@ export default class extends AbstractView {
       navBehav();
     } else {
       this.responseBox.innerHTML = '';
-      this.response = document.createElement('div');
       this.responseStatus = document.createElement('p');
       this.responseStatus.innerText = response.status_code;
       this.responseStatus.classList.add('response-error');
